57463885b82b79d3b25d000f8cea63ae9dd83d30
Sporny <janheidolph@yahoo.de>
2022-01-20 17:51:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ QBConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in ms
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = {['cash'] = 2, ['bank'] = 40} -- ['type']=startamount - Add or remove money types for your server (for ex. ['blackmoney']=0), remember once added it will not be removed from the database!
 QBConfig.Money.DontAllowMinus = {'cash'} -- Money that is not allowed going in minus
-QBConfig.Money.PayCheckTimeOut = 1 -- The time in minutes that it will give the paycheck
+QBConfig.Money.PayCheckTimeOut = 10 -- The time in minutes that it will give the paycheck
 QBConfig.Money.PayCheckSociety = false -- If true paycheck will come from the society account that the player is employed at, requires qb-bossmenu
 
 QBConfig.Player = {}
