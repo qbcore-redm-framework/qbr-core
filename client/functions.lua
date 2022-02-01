200a4c2ef1559380565d75a5cce7078be3305ad7
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-02-01 02:17:20 +0200

    new function DrawTxt

--- client/functions.lua
+++ client/functions.lua
@@ -808,3 +808,13 @@ function QBCore.Functions.Notify(text, textype, length)
         })
     end
 end
+
+function QBCore.Functions.DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+	local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
+	SetTextScale(w, h)
+	SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+	SetTextCentre(centre)
+	if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+	Citizen.InvokeNative(0xADA9255D, 10);
+	DisplayText(str, x, y)
+end
