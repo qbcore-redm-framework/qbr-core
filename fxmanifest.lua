6c73f241e9d293a7391dc55c7fb8ba50ef29f820
DosKit <56743692+DosKit@users.noreply.github.com>
2023-07-01 03:20:42 -0400

    Update Version
    Update Version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'QBCore RedM Edition'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
 	'shared/locale.lua',
