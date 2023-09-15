918768eee5fbdbea7b1dd0ed5f3e616ef1fe1893
DosKit <56743692+DosKit@users.noreply.github.com>
2023-09-15 12:06:48 -0400

    Update events.lua
    Convert QBCore.Shared to QBShared.

--- client/events.lua
+++ client/events.lua
@@ -201,13 +201,13 @@ end)
 
 -- Listen to Shared being updated
 RegisterNetEvent('QBCore:Client:OnSharedUpdate', function(tableName, key, value)
-    QBCore.Shared[tableName][key] = value
+    QBShared[tableName][key] = value
     TriggerEvent('QBCore:Client:UpdateObject')
 end)
 
 RegisterNetEvent('QBCore:Client:OnSharedUpdateMultiple', function(tableName, values)
     for key, value in pairs(values) do
-        QBCore.Shared[tableName][key] = value
+        QBShared[tableName][key] = value
     end
     TriggerEvent('QBCore:Client:UpdateObject')
 end)
