21f29880339395c8a75c9698151dea2220de75a9
Infernalz <51412133+Infernalz92@users.noreply.github.com>
2023-10-01 01:19:52 +0200

    Update loops.lua
    Added: Thread to show/hide horse and player core status/icon

--- client/loops.lua
+++ client/loops.lua
@@ -1,3 +1,6 @@
+local PlayersCore = {ICON_STAMINA = 0, ICON_STAMINA_CORE = 1, ICON_DEADEYE = 2, ICON_DEADEYE_CORE = 3, ICON_HEALTH = 4, ICON_HEALTH_CORE = 5}
+local HorseCore = {ICON_HORSE_HEALTH = 6, ICON_HORSE_HEALTH_CORE = 7, ICON_HORSE_STAMINA = 8, ICON_HORSE_STAMINA_CORE = 9, ICON_HORSE_COURAGE = 10, ICON_HORSE_COURAGE_CORE = 11}
+
 local isLoggedIn = false
 AddStateBagChangeHandler('isLoggedIn', nil, function(bagName, key, value)
     isLoggedIn = value
@@ -22,6 +25,19 @@ CreateThread(function()
                 local currentHealth = GetEntityHealth(ped)
                 SetEntityHealth(ped, currentHealth - math.random(5, 10))
             end
+        end
+    end
+end)
+
+CreateThread(function ()
+    if QBConfig.Hud.HidePlayersCore then
+        for _, v in pairs(PlayersCore) do
+            Citizen.InvokeNative(0xC116E6DF68DCE667, v, 2)
+        end
+    end
+    if QBConfig.Hud.HideHorseCore then
+        for _, v in pairs(HorseCore) do
+            Citizen.InvokeNative(0xC116E6DF68DCE667, v, 2)
         end
     end
 end)
